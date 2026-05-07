4e1e16914b84a1728329c2979a68a372b3291ba5
Yanina Soto <yana_mta_2010@hotmail.com>
2026-05-07 12:33:39 -0300

    idiomas

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
 import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
+  
   locales: ['en', 'es'],
-  defaultLocale: 'en'
+  
+  
+  defaultLocale: 'en',
+  
+  localeDetection: false
 });
 
 export const config = {
+  
   matcher: ['/((?!api|_next|.*\\..*).*)']
 };
