e850f9b51dab5446bab14e57e34e1e636a72bd6d
Yanina Soto <yana_mta_2010@hotmail.com>
2026-05-15 18:31:09 -0300

    googleverify

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,7 +1,6 @@
-'use client';
+
 
 export default function GoogleAnalytics() {
-  // Tu ID de Google Analytics 4
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
   if (!gaId) {
@@ -11,19 +10,20 @@ export default function GoogleAnalytics() {
 
   return (
     <>
-      {/* Google tag (gtag.js) */}
       <script
         async
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-      ></script>
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gaId}');
-        `}
-      </script>
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}');
+          `,
+        }}
+      />
     </>
   );
 }
