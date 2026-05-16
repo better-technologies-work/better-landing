43400970af7c8712a5bfa72c50d77ac97a0f00ae
Yanina Soto <yana_mta_2010@hotmail.com>
2026-05-15 21:15:15 -0300

    fix locale params

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -33,9 +33,9 @@ export default async function RootLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>
 }) {
-  const { locale } = params;
+  const { locale } = await params;
   const messages = await getMessages({ locale });
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
