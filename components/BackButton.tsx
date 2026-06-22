bce1819c7fa090b1bae6331cb4fc634a55cdd7f4
Yanina Soto <yana_mta_2010@hotmail.com>
2026-06-22 20:52:12 -0300

    fixed

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -2,15 +2,14 @@
 
 export default function BackButton({ esText, enText, className, locale }: { esText: string, enText: string, className?: string, locale: string }) {
   
-  // En lugar de window.history.back, vamos a redirigir a una URL clara.
-  // Esto elimina el problema de la carga infinita.
+  
   const targetUrl = locale === 'en' ? '/blog' : `/${locale}/blog`;
 
   return (
     <a 
       href={targetUrl}
       className={className}
-      // Usamos el evento solo para asegurar que no haya comportamientos raros
+      
     >
       {locale === 'es' ? `← ${esText}` : `← ${enText}`}
     </a>
